faed5573844e82b22099a5b115b0d5bb3bb4e649
mohammed-bn <mbentiss11@gmail.com>
2025-09-28 21:28:35 +0100

    mini projet version 2

---
+++ header.h
@@ -0,0 +1,13 @@
+#include "fonction.c"
+
+
+char* strcapy(char source[], char distination[]);
+void ajoute();
+void modifier();
+void Suppression();
+void affichage();
+void recherche();
+
+
+// void menu();
+
---
+++ v_global.h
@@ -0,0 +1,7 @@
+int id_avion[1000];
+char modele[1000][30];
+int capacite[1000];
+char status[1000][20];
+char dateEentree[1000][10];
+int n_ajoute; // le nomber d'avion que enter par l'utilisateur
+int jour, moi, annee; 
